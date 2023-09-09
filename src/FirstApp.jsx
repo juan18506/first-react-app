b1e0d2e480c67f5bad59ff29cfa46afbd1349e7f
juan18506 <juan185067@gmail.com>
2023-09-09 01:11:01 -0300

    Used { } to render expressions

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,8 +1,22 @@
+const newMessage = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const getResult = (a, b) => {
+  return a + b;
+}
+
+// Promise object => render error
+const getResultAsync = async (a, b) => {
+  return a + b;
+}
+
 export const FirstApp = () => {
   return (
     <>
-      <h1>Juan</h1>
-      <p>A Subtitle</p>
+      <h1>My First App</h1>
+      <h2>A Subtitle</h2>
+      <p>{ newMessage }</p>
+      <p>{ getResult(7, 2) }</p>
+      {/* <p>{ getResultAsync(7, 2) }</p> */}
     </>
   )
 }
