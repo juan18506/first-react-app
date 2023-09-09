d4701b322c67b28933f8ee6c85fbc8306ccc03d2
juan18506 <juan185067@gmail.com>
2023-09-09 14:24:19 -0300

    Created CounterApp component

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp';
 import './styles.css'
+import { CounterApp } from './CounterApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp title='My First App' />
+    <CounterApp value={0} />
   </React.StrictMode>
 );
