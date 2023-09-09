4177aad286e7c3e1b97be408a17dce2cc8b1932b
juan18506 <juan185067@gmail.com>
2023-09-09 02:00:01 -0300

    Added propTypes to FirstApp component

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 const newMessage = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const getResult = (a, b) => {
@@ -5,20 +7,26 @@ const getResult = (a, b) => {
 }
 
 // Promise object => render error
-const getResultAsync = async (a, b) => {
-  return a + b;
-}
+// const getResultAsync = async (a, b) => {
+//   return a + b;
+// }
 
-export const FirstApp = ({ title, subTitle = 'A Subtitle' }) => {
+export const FirstApp = ({ title, version }) => {
   // console.log(props);
 
   return (
     <>
       <h1>{ title }</h1>
-      <h2>{ subTitle }</h2>
+      <h2>A Subtitle</h2>
+      <p>{ version }</p>
       <p>{ newMessage }</p>
       <p>{ getResult(7, 2) }</p>
       {/* <p>{ getResultAsync(7, 2) }</p> */}
     </>
   )
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  version: PropTypes.number
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp.jsx';
+import { FirstApp } from './FirstApp';
 import './styles.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp title="My First App" />
+    <FirstApp title='My First App' />
   </React.StrictMode>
 );
