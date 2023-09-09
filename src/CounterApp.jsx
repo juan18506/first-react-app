72e003121f5b0c9c5a7400db33d00bedb7bfd594
juan18506 <juan185067@gmail.com>
2023-09-09 15:14:13 -0300

    Added useState hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const handleAdd = () => console.log('+1');
 
 export const CounterApp = ({ value }) => {
+  const [ counter, setCounter ] = useState( value );
+
+  const handleAdd = () => {
+    setCounter( counter + 1 );
+    // setCounter( (c) => c + 1 );
+  }
+  
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> { value } </h2>
+      <h2> { counter } </h2>
 
       <button onClick={ handleAdd }>
         +1
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,6 @@ import { CounterApp } from './CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp value={0} />
+    <CounterApp value={ 10 } />
   </React.StrictMode>
 );
