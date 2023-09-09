35cb3f0886c5d510789215f43d3144a454f2b2d5
juan18506 <juan185067@gmail.com>
2023-09-09 14:38:37 -0300

    Added click event

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types';
 
+const handleAdd = () => console.log('+1');
+
 export const CounterApp = ({ value }) => {
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{ value }</h2>
+      <h2> { value } </h2>
+
+      <button onClick={ handleAdd }>
+        +1
+      </button>
     </>
   );
 }
