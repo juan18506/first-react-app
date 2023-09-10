ff197cd1c61805ec3951cae53742c34b76559559
juan18506 <juan185067@gmail.com>
2023-09-09 21:05:51 -0300

    setup jsdom as vitest environment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   base: "/react-counter-app/",
   plugins: [react()],
   test: {
-    setupFiles: ['./vitest.setup.js'],
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.js"],
   },
 })
